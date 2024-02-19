3d14da5c8a1bf380f14a6a34dc29094fb66df189
Ashwin Yadav <104228697+ashwin7455@users.noreply.github.com>
2024-02-19 22:58:30 +0530

    hbh

--- src/Components/StyleSheet.js
+++ src/Components/StyleSheet.js
@@ -5,7 +5,7 @@ function StyleSheet(props) {
     let className = props.primary ? 'primary' : ''
   return (
     <div className= {`${className} font-xl`}>
-     <h1>StyleSheet</h1> 
+     {/* <h1>StyleSheet</h1>  */}
     </div>
   )
 }
